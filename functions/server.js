ae2b75d67f204763b1d040b6829fb5b5b2fc48e6
Manoj E S <manoj.es.professional@gmail.com>
2023-10-04 01:21:24 +0530

    Pre-deployment commit 7

--- functions/server.js
+++ functions/server.js
@@ -15,8 +15,8 @@ const utilityFunctions = require(path.join(__dirname, '/utilityFunctions'));
 
 /////////////////////////////////////////////////////////////////////////////// Global Variables and Constants //
 
-const PORT = 3000 || process.env.PORT;
-const ServerURL = 'http://localhost:3000/';
+const PORT = process.env.PORT || 3000;
+const ServerURL = process.env.SERVER_URL || 'https://todotype-mes.netlify.app' || 'http://localhost:3000/';
 const app = express();
 
 var count = 0;
